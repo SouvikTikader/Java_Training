033071bcbdc697b4d126a2bc8211c2f9c5ce8ec3
SouvikTikader <142192788+SouvikTikader@users.noreply.github.com>
2026-06-11 10:54:01 +0530

    Add files via upload

---
+++ Day1/To7.java
@@ -0,0 +1,11 @@
+public class To7 {
+  public static void main(String[] args) {
+    int num1 = 7;
+
+    for(int i=0; i<10; i++){
+      System.out.println(num1+" X "+(i+1)+" = "+(num1*(i+1)));
+    
+    }
+  }
+  
+}
---
+++ Day1/souvik.java
@@ -0,0 +1,9 @@
+public class souvik {
+  public static void main(String[] args) {
+    System.out.println("Hello, Souvik!");
+    for(int i=0; i<5; i++){
+      System.out.println("Souvik is learning Java.");
+    }
+  }
+
+}
